5537351222d8f098eb0cd73e7a27b4f736c6ca4c
Lassi Paavolainen <lassi.paavolainen@gmail.com>
2020-11-29 01:42:13 +0200

    Fixed issues with smaller images than 224x224

--- wrapper.py
+++ wrapper.py
@@ -2,6 +2,8 @@ import sys
 import os
 import shutil
 import subprocess
+import imageio
+import numpy as np
 from cytomine.models import Job
 from biaflows import CLASS_OBJSEG, CLASS_SPTCNT, CLASS_PIXCLA, CLASS_TRETRC, CLASS_LOOTRC, CLASS_OBJDET, CLASS_PRTTRK, CLASS_OBJTRK
 from biaflows.helpers import BiaflowsJob, prepare_data, upload_data, upload_metrics, get_discipline
@@ -18,19 +20,47 @@ def main(argv):
         # 1. Prepare data for workflow
         in_imgs, gt_imgs, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, bj, is_2d=True, **bj.flags)
 
+        # Make sure all images have at least 224x224 dimensions
+        # and that minshape / maxshape * minshape >= 224
+        resized = {}
+        for bfimg in in_imgs:
+            fn = os.path.join(in_path, bfimg.filename)
+            img = imageio.imread(fn)
+            minshape = min(img.shape[:2])
+            maxshape = max(img.shape[:2])
+            if minshape != maxshape or minshape < 224:
+                resized[bfimg.filename] = img.shape
+                padshape = []
+                for i in range(2):
+                    if img.shape[i] < max(224,maxshape):
+                        padshape.append((0,max(224,maxshape)-img.shape[i]))
+                    else:
+                        padshape.append((0,0))
+                if len(img.shape) == 3:
+                    padshape.append((0,0))
+                img = np.pad(img, padshape, 'constant', constant_values=0)
+            imageio.imwrite(os.path.join(tmp_path, bfimg.filename), img)
+
         # 2. Run image analysis workflow
         bj.job.update(progress=25, statusComment="Launching workflow...")
 
         # Add here the code for running the analysis script
-        cmd = ["python", "-m", "cellpose", "--dir", in_path, "--pretrained_model", "nuclei", "--save_tif", "--all_channels", "--diameter", "{:f}".format(bj.parameters.diameter), "--cellprob_threshold", "{:f}".format(bj.parameters.prob_threshold)]
+        cmd = ["python", "-m", "cellpose", "--dir", tmp_path, "--pretrained_model", "nuclei", "--save_tif", "--no_npy", "--all_channels", "--diameter", "{:f}".format(bj.parameters.diameter), "--cellprob_threshold", "{:f}".format(bj.parameters.prob_threshold)]
         status = subprocess.run(cmd)
 
         if status.returncode != 0:
             print("Running Cellpose failed, terminate")
             sys.exit(1)
 
+        # Crop to original shape
         for bimg in in_imgs:
-            shutil.copy(os.path.join(in_path,bimg.filename_no_extension+"_cp_masks.tif"), os.path.join(out_path,bimg.filename))
+            shape = resized.get(bimg.filename, None)
+            if shape:
+                img = imageio.imread(os.path.join(tmp_path,bimg.filename_no_extension+"_cp_masks.tif"))
+                img = img[0:shape[0], 0:shape[1]]
+                imageio.imwrite(os.path.join(out_path,bimg.filename), img)
+            else:
+                shutil.copy(os.path.join(tmp_path,bimg.filename_no_extension+"_cp_masks.tif"), os.path.join(out_path,bimg.filename))
         
         # 3. Upload data to BIAFLOWS
         upload_data(problem_cls, bj, in_imgs, out_path, **bj.flags, monitor_params={
